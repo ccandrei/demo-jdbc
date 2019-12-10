cb1e7b05b43bcff61a4bef7b5a694f1b65b5afdb
ccandrei <craciunescu.andrei@gmail.com>
2019-12-10 14:37:52 +0100

    v2.1

--- src/main/java/fr/diginamic/props/TestConfiguration.java
+++ src/main/java/fr/diginamic/props/TestConfiguration.java
@@ -7,7 +7,7 @@ public class TestConfiguration {
 	public static void main(String[] args) {
 		
 		ResourceBundle demojdbc = ResourceBundle.getBundle("demojdbc");
-		String valeur = demojdbc.getString("cle1");
+		String valeur = demojdbc.getString("connection");
 		String valeur1 = demojdbc.getString("user");
 		System.out.println(valeur);
 		System.out.println(valeur1);
--- src/main/java/fr/diginamic/props/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/props/TestConnexionJdbc.java
@@ -1,10 +1,34 @@
 package fr.diginamic.props;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		}catch (ClassNotFoundException e) {
+			System.out.println("Driver non trouve!");
+		}
+			
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/compta", "root", "");
+			
+			System.out.println(connection.getClass());
+			System.out.println(connection.getCatalog());
+			
+			connection.close();
+		}catch (SQLException e) {
+			System.out.println("Connexion impossible à établir.");
+		}
+			
+			
+	
+		
 	}
 
 }
