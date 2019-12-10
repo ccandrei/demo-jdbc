88c648f11f93f363ec81cbdf9263dbd137e95b03
ccandrei <craciunescu.andrei@gmail.com>
2019-12-10 11:55:38 +0100

    v.2

---
+++ src/main/java/fr/diginamic/props/TestConfiguration.java
@@ -0,0 +1,16 @@
+package fr.diginamic.props;
+
+import java.util.ResourceBundle;
+
+public class TestConfiguration {
+
+	public static void main(String[] args) {
+		
+		ResourceBundle demojdbc = ResourceBundle.getBundle("demojdbc");
+		String valeur = demojdbc.getString("cle1");
+		String valeur1 = demojdbc.getString("user");
+		System.out.println(valeur);
+		System.out.println(valeur1);
+	}
+
+}
